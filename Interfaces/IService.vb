4a6b6698a2a22a09692c1cf1e37c7b42f6e03918
kourt3 <kourt35@gmail.com>
2026-08-14 10:24:06 +0300

    - Add Service for Models - Fixed Names of services - Add More Interface of Services - Add and Fixed descriptions
    its biggest Realese and other projects need change the services.
    
    closes #3

--- Interfaces/IService.vb
+++ Interfaces/IService.vb
@@ -2,25 +2,24 @@
 Imports FoundationLibrary.Interfaces.ValMsg
 Namespace Interfaces.Service
     ''' <summary>
-    ''' <para>O service έλενχει αν επιτρέπεται να κανει καποια ενεργεια στην Βάση δεδομένων.</para>
-    ''' <em>Για να λειτουργείσει ο Service και να επικοινωνηση με το Αποθετήριο θα πρέπει στην βάση Δεδομένων να υπαρχει το αντιστοιχο κλειδι <see cref="Interfaces.Keys.IHasPrimaryKey(Of T)"/></em>
+    ''' Τα βασικά κλειδια που θα χρειαστεις για να διμιουργήσεις Service
     ''' </summary>
-    ''' <typeparam name="Tkey">Τύπος PK</typeparam>
-    ''' <typeparam name="TData">Data με αναφορα κλειδιου <see cref="Interfaces.Keys.IHasPrimaryKey(Of T)"/></typeparam>
-    Public Interface IService(Of Tkey, TData As IHasPrimaryKey(Of Tkey))
+    ''' <typeparam name="TRef">Η αναγνώρηση του κλιδειου(PK)</typeparam>
+    ''' <typeparam name="TReturn">Τι να επιστρέψει μεσο του <seealso cref="IvalMsg(Of TReturn)"/></typeparam>
+    Public Interface IKeysServices(Of TRef, TReturn)
         ''' <summary>
         ''' Κάνει έλενχο αν υπάρχει στο αποθετήριο η σηγκεκριμένη εγραφή και αν το επιτρέπεται να επιστραφή η εγραφή.
         ''' </summary>
         ''' <param name="Ref">Data</param>
-        ''' <returns>Επιστρέφει <seealso cref="ValMsg.IValMsg(Of IModel)"/></returns>
-        Function Exist(Ref As TData) As IValMsg(Of TData)
+        ''' <returns>Επιστρέφει <seealso cref="ValMsg.IValMsg(Of TReturn)"/></returns>
+        Function Exist(Ref As TRef) As IValMsg(Of TReturn)
         ''' <summary>
         ''' ο Service κάνει έλενχο αν μπορει και αν επιτρέπεται να κανει εγραφή στο Αποθετήριο περνώντας τα δεδομένα απο το DTO.
         ''' </summary>
         ''' <typeparam name="DTO">Data Transfer Object</typeparam>
         ''' <param name="RegisterDTO">Register Data Transfer Object</param>
-        ''' <returns>Επιστρέφει <seealso cref="IValMsg(Of IModel)"/></returns>
-        Function Register(Of DTO)(RegisterDTO As DTO) As IValMsg(Of TData)
+        ''' <returns>Επιστρέφει <seealso cref="IValMsg(Of TReturn)"/></returns>
+        Function Register(Of DTO)(RegisterDTO As DTO) As IValMsg(Of TReturn)
         ''' <summary>
         ''' ο Service κάνει έλενχο αμα μπορει και επιτρέπεται να να κανει καποια αλλαγή μεσα στα δεδομένα.
         ''' </summary>
@@ -28,17 +27,38 @@ Namespace Interfaces.Service
         ''' <param name="Ref"></param>
         ''' <param name="ChangeDTO"></param>
         ''' <returns>Επιστρέφει <seealso cref="IValMsg"/></returns>
-        Function Change(Of DTO)(Ref As TData, ChangeDTO As DTO) As IValMsg
+        Function Change(Of DTO)(Ref As TRef, ChangeDTO As DTO) As IValMsg
         ''' <summary>
         ''' o Service Κανει ελενχο αν μπορει να διαγραφή η συγκεκριμένη εγραφή.
         ''' </summary>
         ''' <param name="Ref">DATA</param>
         ''' <returns>Επιστρέφει <seealso cref="IValMsg"/></returns>
-        Function Remove(Ref As TData) As IValMsg
+        Function Remove(Ref As TRef) As IValMsg
         ''' <summary>
         ''' o Service Ελέχνει αμα μπορει να δωσει όλες της εγραφες στον χρήστη.
         ''' </summary>
-        ''' <returns>Επιστρέφει <seealso cref="IValMsg"/></returns>
-        Function Get_All() As IValMsg(Of List(Of TData))
+        ''' <returns>Επιστρέφει <seealso cref="IValMsg(Of List(Of TReturn))"/></returns>
+        Function Get_All() As IValMsg(Of List(Of TReturn))
+
+    End Interface
+
+
+    ''' <summary>
+    ''' <Title>Service που Επιστρέφει Model</Title>
+    ''' <para>O service έλενχει αν επιτρέπεται να κανει καποια ενεργεια στην Βάση δεδομένων.</para>
+    ''' </summary>
+    ''' <typeparam name="TModel">Model</typeparam>
+    ''' <typeparam name="TData">Data</typeparam>
+    Public Interface IService(Of TData, TModel)
+        Inherits IKeysServices(Of TData, TModel)
+    End Interface
+
+    ''' <summary>
+    ''' <Title>Service που Επιστρέφει το ιδιο Entity</Title>
+    ''' <para>O service έλενχει αν επιτρέπεται να κανει καποια ενεργεια στην Βάση δεδομένων.</para>
+    ''' </summary>
+    ''' <typeparam name="TData">Data"/></typeparam>
+    Public Interface IService(Of TData)
+        Inherits IKeysServices(Of TData, TData)
     End Interface
 End Namespace
--- Services/ServiceCE.vb
+++ Services/ServiceCE.vb
@@ -6,22 +6,37 @@ Imports FoundationLibrary.ValMsg
 Namespace Services
 
     ''' <summary>
-    ''' O Services Επιστρέφει ενα αντιγραφο του Entity
+    '''<Title>O Services Επιστρέφει ενα αντιγραφο του Entity</Title>
+    '''
+    '''<para><em>
+    ''' Για να λειτουργείσει ο Service και να επικοινωνισει με το Αποθετήριο θα πρέπει στην βάση Δεδομένων να υπαρχει στο <typeparamref name="TEntity"/> το αντιστοιχο κλειδι <see cref="Interfaces.Keys.IHasPrimaryKey(Of T)"/>
+    ''' </em></para>
+    ''' 
+    ''' <important>
+    ''' <b>* Θα χρειαστεί να υλοποιησεις Την <see cref="MemberizeClone">MemberizeClone()</see></b> 
+    ''' </important>
+    ''' 
     ''' </summary>
     ''' <typeparam name="Tkey"></typeparam>
     ''' <typeparam name="TEntity"></typeparam>
     ''' <typeparam name="TRepository"></typeparam>
-    Public MustInherit Class ServiceCE(Of Tkey, TEntity As IHasPrimaryKey(Of Tkey), TRepository As IRepository(Of Tkey, TEntity))
-        Implements IService(Of Tkey, TEntity)
+    Public MustInherit Class ServiceCloneEntity(Of Tkey, TEntity As IHasPrimaryKey(Of Tkey), TRepository As IRepository(Of Tkey, TEntity))
+        Implements IService(Of TEntity)
 
         Public Property Repository As TRepository
+
         Sub New(ReposirotyLink As TRepository)
             Repository = ReposirotyLink
         End Sub
+        ''' <summary>
+        ''' Δημιουργει ενα αντιγραφό του Entity
+        ''' </summary>
+        ''' <param name="Enity"></param>
+        ''' <returns></returns>
         MustOverride Function MemberizeClone(Enity As TEntity) As TEntity
         MustOverride Function ToEntity(Of TDTO)(DTO As TDTO) As TEntity
         MustOverride Function ToEntity(Of TDTO)(DTO As TDTO, Entity As TEntity) As TEntity
-        Public Overridable Function Exist(Ref As TEntity) As IValMsg(Of TEntity) Implements IService(Of Tkey, TEntity).Exist
+        Public Overridable Function Exist(Ref As TEntity) As IValMsg(Of TEntity) Implements IService(Of TEntity).Exist
             Dim Result As New ValMsg(Of TEntity)
             Dim Entity As TEntity = Repository.Read_Item(Ref.PrimaryKey)
 
@@ -37,7 +52,7 @@ Namespace Services
             Return Result
         End Function
 
-        Public Overridable Function Register(Of DTO)(RegisterDTO As DTO) As IValMsg(Of TEntity) Implements IService(Of Tkey, TEntity).Register
+        Public Overridable Function Register(Of DTO)(RegisterDTO As DTO) As IValMsg(Of TEntity) Implements IService(Of TEntity).Register
             Dim Val As New ValMsg(Of TEntity)
 
             Dim Entity As TEntity = ToEntity(RegisterDTO)
@@ -53,7 +68,7 @@ Namespace Services
             End If
         End Function
 
-        Public Overridable Function Change(Of DTO)(Ref As TEntity, ChangeDTO As DTO) As IValMsg Implements IService(Of Tkey, TEntity).Change
+        Public Overridable Function Change(Of DTO)(Ref As TEntity, ChangeDTO As DTO) As IValMsg Implements IService(Of TEntity).Change
             Dim Val As New ValMsg.ValMsg
 
             Dim Entity As TEntity = Repository.Read_Item(Ref.PrimaryKey)
@@ -68,7 +83,7 @@ Namespace Services
             Return Val
         End Function
 
-        Public Overridable Function Remove(Ref As TEntity) As IValMsg Implements IService(Of Tkey, TEntity).Remove
+        Public Overridable Function Remove(Ref As TEntity) As IValMsg Implements IService(Of TEntity).Remove
             Dim Val As New ValMsg.ValMsg
             If Repository.Delete(Ref.PrimaryKey) Then
                 Val.Success = True
@@ -80,7 +95,7 @@ Namespace Services
             Return Val
         End Function
 
-        Public Overridable Function Get_All() As IValMsg(Of List(Of TEntity)) Implements IService(Of Tkey, TEntity).Get_All
+        Public Overridable Function Get_All() As IValMsg(Of List(Of TEntity)) Implements IService(Of TEntity).Get_All
             Dim Val As New ValMsg(Of List(Of TEntity))
             Val.Model = New List(Of TEntity)
             For Each Entity In Repository.Read_All
